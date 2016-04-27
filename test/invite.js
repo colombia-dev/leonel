483f5b91042a5b42af3a9156cd25b890c715529d
Juan Pablo Buritica <buritica@gmail.com>
2016-04-26 23:52:59 -0400

    [add] guest storage

--- test/invite.js
+++ test/invite.js
@@ -8,13 +8,38 @@ import querystring from 'querystring';
 // setup good invitation test
 test.beforeEach(t => {
   let guest = 'buritica@gmail.com';
+  let userID = 'userID';
+
+  // configure storage stubs
+  let hostData = {
+    id: userID,
+    guests: ['previous@gmail.com'],
+  };
+
+  let storage =  {
+    users: {
+      get: sinon.stub(),
+      save: sinon.stub(),
+    },
+  };
+
+  storage.users.get.callsArgWith(1, null, hostData);
+  storage.users.save.callsArg(1);
+
+  // export context
   t.context = {
     guest,
     bot: {
       reply: sinon.spy(),
+      botkit: {
+        storage: storage,
+      },
     },
     message: {
       match: [`invite a ${guest}`, `${guest}`],
+      user: {
+        id: userID,
+      },
     },
   };
 });
@@ -47,11 +72,29 @@ test.cb('it replies to new invitation success', t => {
     );
 
     t.true(calledWith, 'bot replied');
-    t.end();
+    t.end(null);
   });
 });
 
-test.todo('it logs invitation in users storage');
+test.cb.only('it logs invitation in hosts storage', t => {
+  let { bot, message, guest, email } = t.context;
+  let { storage } = bot.botkit;
+  t.plan(3);
+  nock('https://colombia-dev.slack.com')
+    .post('/api/users.admin.invite')
+    .reply(200, { ok: true });
+
+  invite(bot, message, function () {
+    let getCalledWith = storage.users.get.calledWith(message.user);
+    let [previousGuest, newGuest] = storage.users.save.args[0][0].guests;
+
+    t.true(getCalledWith, 'finds host data');
+    t.is(newGuest, guest, `logged guest is ${email}`);
+    t.is(previousGuest, 'previous@gmail.com', `logged guest is ${previousGuest}`);
+    t.end(null);
+  });
+
+});
 
 test.todo('it creates storage if it doesn\'t exist yet');
 
