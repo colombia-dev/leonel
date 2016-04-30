a36f2d5d8592548861be6d49a02c37e7478f213f
Juan Pablo Buritica <buritica@gmail.com>
2016-04-29 20:06:44 -0400

    [fix] jscs warnings

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
-var Botkit = require('botkit');
-var invite = require('./lib/invite');
-var storage = require('botkit-storage-mongo')({mongoUri: process.env.MONGO_URI});
+let Botkit = require('botkit');
+let invite = require('./lib/invite');
+let storage = require('botkit-storage-mongo')({ mongoUri: process.env.MONGO_URI });
 
 // Expect a SLACK_TOKEN environment variable
-var slackToken = process.env.SLACK_TOKEN;
+let slackToken = process.env.SLACK_TOKEN;
 if (!slackToken) {
   console.error('SLACK_TOKEN is required!');
   process.exit(1);
 }
 
-var controller = Botkit.slackbot({
+let controller = Botkit.slackbot({
   storage: storage,
 });
 
-var bot = controller.spawn({
+let bot = controller.spawn({
   token: slackToken,
 });
 
 bot.startRTM(function (err, bot, payload) {
-  if (err) {
-    throw new Error('Could not connect to Slack');
-  }
+  if (err) { throw new Error('Could not connect to Slack'); }
 });
 
 controller.on('bot_channel_join', function (bot, message) {
-  bot.reply(message, "I'm here!");
+  bot.reply(message, 'I\'m here!');
 });
 
 controller.hears('invite a <mailto:(.*)\\|.*>', ['direct_message'], invite);
 
 controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
-  var help = 'I will respond to the following messages: \n' +
+  let help = 'I will respond to the following messages: \n' +
       '`bot hi` for a simple message.\n' +
       '`bot attachment` to see a Slack attachment message.\n' +
       '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
