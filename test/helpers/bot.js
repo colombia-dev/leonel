a7a7665ab5d4795345fc77f2b2aef4531fe67dbd
Juan Pablo Buritica <buritica@gmail.com>
2016-05-02 23:30:19 -0500

    [fix] use bot spies instead of stubs

--- test/helpers/bot.js
+++ test/helpers/bot.js
@@ -1,20 +1,37 @@
 'use strict';
 let sinon = require('sinon');
 let debug = require('debug')('helper:bot');
+let noop = () => {};
+
+function say(opts, cb) {
+  debug('say');
+  cb = cb || noop;
+  cb(null);
+}
+
+function reply(message, content, cb) {
+  debug('reply');
+  cb = cb || noop;
+  cb(null);
+}
 
 function Bot(opts) {
   opts = opts || {};
 
   debug('init', opts);
   let storage = opts.storage || {};
-
-  return {
-    reply: sinon.stub().yields(),
-    say: sinon.stub().yields(),
+  let bot = {
+    reply,
+    say,
     botkit: {
       storage,
     },
   };
+
+  sinon.spy(bot, 'reply');
+  sinon.spy(bot, 'say');
+
+  return bot;
 }
 
 module.exports = Bot;
