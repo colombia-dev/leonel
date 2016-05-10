d39c088cc86fb798c3ef8a220ca311877f4df938
Juan Pablo Buritica <buritica@gmail.com>
2016-05-09 22:34:13 -0400

    [add] seed script

---
+++ scripts/seed.js
@@ -0,0 +1,37 @@
+'use strict';
+
+let Promise = require('bluebird');
+let moment = require('moment');
+let Botkit = require('botkit');
+let storage = require('botkit-storage-mongo')({ mongoUri: process.env.MONGO_URI });
+let updates = [];
+
+// Expect a SLACK_TOKEN environment variable
+let token = process.env.SLACK_TOKEN;
+if (!token) {
+  console.error('SLACK_TOKEN is required!');
+  process.exit(1);
+}
+
+let controller = Botkit.slackbot({ storage });
+let bot = controller.spawn({ token });
+let userSave = Promise.promisify(bot.botkit.storage.users.save);
+
+bot.api.users.list({}, (err, res) => {
+  res.members.forEach(member => {
+    console.log('%s', member.name);
+    let user = {
+      id: member.id,
+      name: member.name,
+      guests: [],
+      createdAt: moment().subtract(46, 'days').toDate(),
+    };
+
+    updates.push(userSave(user));
+  });
+
+  Promise.all(updates).then(() => {
+    console.log('%s users updated', updates.length);
+    process.exit(0);
+  });
+});
