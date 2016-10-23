25784c346381203062a14a6f8bc226b38ddeba4a
Juan Pablo Buritica <buritica@gmail.com>
2016-10-23 18:11:20 -0400

    [standardize] index.js

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-'use strict';
+'use strict'
 
-const config = require('./lib/config');
-const checkConfig = require('./lib/check-config');
-checkConfig(config, ['SLACK_TOKEN', 'SLACK_ADMIN_TOKEN', 'SLACK_TEAM_NAME', 'DEBUG', 'MONGO_URI', 'CHANNEL_INTROS']);
+const config = require('./lib/config')
+const checkConfig = require('./lib/check-config')
+checkConfig(config, ['SLACK_TOKEN', 'SLACK_ADMIN_TOKEN', 'SLACK_TEAM_NAME', 'DEBUG', 'MONGO_URI', 'CHANNEL_INTROS'])
 
-const Botkit = require('botkit');
-const invite = require('./lib/invite');
-const onboard = require('./lib/onboard');
-const guests = require('./lib/guests');
-const storage = require('botkit-storage-mongo')({ mongoUri: config.MONGO_URI });
-const debug = require('debug')('bot:main');
-const packageInfo = require('./package.json');
+const Botkit = require('botkit')
+const invite = require('./lib/invite')
+const onboard = require('./lib/onboard')
+const guests = require('./lib/guests')
+const storage = require('botkit-storage-mongo')({ mongoUri: config.MONGO_URI })
+const debug = require('debug')('bot:main')
+const packageInfo = require('./package.json')
 
 // Expect a SLACK_TOKEN environment variable
-let slackToken = config.SLACK_TOKEN;
+let slackToken = config.SLACK_TOKEN
 if (!slackToken) {
-  console.error('SLACK_TOKEN is required!');
-  process.exit(1);
+  console.error('SLACK_TOKEN is required!')
+  process.exit(1)
 }
 
 let controller = Botkit.slackbot({
-  storage: storage,
-});
+  storage: storage
+})
 
 let bot = controller.spawn({
-  token: slackToken,
-});
+  token: slackToken
+})
 
 bot.startRTM((err, bot, payload) => {
-  if (err) { throw new Error('Could not connect to Slack'); }
-});
+  if (err) { throw new Error('Could not connect to Slack') }
+})
 
 controller.on('bot_channel_join', (bot, message) => {
-  bot.reply(message, '¡Listo papito, si es ya, es ya!');
-});
+  bot.reply(message, '¡Listo papito, si es ya, es ya!')
+})
 
 /**
  * Coqueto ;)
@@ -42,17 +42,17 @@ controller.hears(['coqueto'], ['direct_mention', 'direct_message'], (bot, messag
   bot.reply(
     message,
     'Yo no soy coqueto... soy un tierno. https://www.youtube.com/watch?v=sFpdl0EiLkA&feature=youtu.be&t=223'
-  );
-});
+  )
+})
 
 /**
  * Invitations
  */
-controller.hears('invite a <mailto:(.*)\\|.*>', 'direct_message', invite);
+controller.hears('invite a <mailto:(.*)\\|.*>', 'direct_message', invite)
 
 controller.hears('invite', 'direct_mention', (bot, message) => {
-  bot.reply(message, 'Invitaciones por DM por favor :soccer:');
-});
+  bot.reply(message, 'Invitaciones por DM por favor :soccer:')
+})
 
 /**
  * Private Onboarding
@@ -60,12 +60,12 @@ controller.hears('invite', 'direct_mention', (bot, message) => {
  * so an event with the same info fires, since we don't have a way to simulate
  * slack events easily right now
  */
-controller.on('team_join', onboard);
+controller.on('team_join', onboard)
 
 /**
  * Invited guests
  */
-controller.hears(['parceros', 'llaverias', 'neas', 'ñeros', 'invitados'], 'direct_message', guests);
+controller.hears(['parceros', 'llaverias', 'neas', 'ñeros', 'invitados'], 'direct_message', guests)
 
 /**
  * Help
@@ -75,22 +75,22 @@ controller.hears(['help', 'ayuda'], ['direct_message', 'direct_mention'], (bot, 
     'Yo respondo a:',
     '- `/dm @leonel invite a me@example.com` para enviar una invitación a este Slack.',
     '- `@leonel ayuda/help` para ver este mensaje.',
-    `... y me podés estender en ${packageInfo.homepage}`,
-  ].join('\n');
-  bot.reply(message, help);
-});
+    `... y me podés estender en ${packageInfo.homepage}`
+  ].join('\n')
+  bot.reply(message, help)
+})
 
 /**
  * Channel Debugging
  */
 controller.hears('test', ['direct_mention', 'direct_message'], (bot, message) => {
-  debug('message', JSON.stringify(message, null, 2));
-  bot.reply(message, 'testing');
-});
+  debug('message', JSON.stringify(message, null, 2))
+  bot.reply(message, 'testing')
+})
 
 /**
  * Uncaught Messages
  */
 controller.hears('.*', ['direct_message', 'direct_mention'], (bot, message) => {
-  debug('not caught', JSON.stringify(message, null, 2));
-});
+  debug('not caught', JSON.stringify(message, null, 2))
+})
