6437c19b4d5132fd2676c129b7941772a0cf0bf1
Juan Pablo Buriticá <buritica@gmail.com>
2021-05-30 20:52:52 -0400

    [remove] coin script the api for coincap changed significantly, and this script broke a long time ago.

--- middlewares/coinMiddleware.js
+++
@@ -1,9 +0,0 @@
-function coinMiddleware (patterns, message) {
-  const re = /(BTC|BCH|ETH|LTC|XRP|ADA|IOT|XEM|XLM|DASH)/gi
-  if (re.test(message.text)) {
-    return true
-  }
-  return false
-}
-
-module.exports = coinMiddleware
